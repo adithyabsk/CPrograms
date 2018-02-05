9c8889f01873d11953a998ea06e90e06150e227d
Adithya Balaji <adithyabsk@gmail.com>
2018-02-05 14:45:34 -0500

    Fix binary string function and add comments to code

--- Calculator/calc.c
+++ Calculator/calc.c
@@ -1,38 +1,53 @@
-// TODO Add comments and header
-#include <stdio.h>
-#include <stdlib.h>
+/* calc --- A simple calculator with multiple features
+ * Usage --- ./calc
+ * Adithya Balaji, 02-05-2018
+ * Tested on a EOS Linux System
+ */
+
+#include <ctype.h>
+#include <float.h>
 #include <limits.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <float.h>
-#include <ctype.h>
 
+/**
+ *	A function that finds the binary representation of an integer
+ *	Args:
+ *		a (int): The integer that is to be binarized
+ *		bin_str (char**): A string pointer is allocated representing the string to 
+ *						  represent the binary form of an integer
+ *						  Note: callee must deallocate bin_str
+ */
 void binaryString(int a, char** bin_str)
 {
-	int bit_count = sizeof(int) * CHAR_BIT;
-	*bin_str = (char*)malloc((bit_count+1) * sizeof(char));
+	int bit_count = sizeof(int) * CHAR_BIT; // max size of string rep of int
+	*bin_str = (char*)malloc((bit_count+1) * sizeof(char)); // alloc string pointer
+	memset(*bin_str, 0, bit_count+1); // set string to all null chars
 	int i = 0;
 	int arr_pos = 0;
-	int first_bit = 0;
 	int should_skip = 1;
 	for(i = 0; i < bit_count; i++)
 	{
+		// A binary mask that checks each bit one by one and converts to the ASCII
+		// representation of that bit
 		char temp = (char)(((a >> (bit_count - 1 - i)) & 1) + 48);
-		if(i == 0)
-			first_bit = temp;
-		else
-		{	if(should_skip)
-			{
-				if(temp == first_bit) continue;
-				else should_skip = 0;
-			}
-		}
+		if(temp == '1')
+			should_skip = 0;
+		if(should_skip) continue;
 		(*bin_str)[arr_pos] = temp;
 		arr_pos++;
 	}
-	(*bin_str)[i] = '\0';
+	if(arr_pos == 0) (*bin_str)[arr_pos++] = '0'; // if a is zero set it to be so
 }
 
+/**
+ *	A function that interprets and processes the requested operation
+ *	Args:
+ *		nums (double[]): An array of doubles representing the last two inputs
+ *		op (char): A character representing the requested operation
+ */
 void calculate(double nums[], char op)
 {
 	switch(op)
@@ -114,7 +129,8 @@ void calculate(double nums[], char op)
 			printf("a: adds the values --> later += earlier\n");
 			printf("s: subtracts the values --> later -= earlier\n");
 			printf("^: exponentiates the values --> later = pow(later, earlier)\n");
-			printf("l: computes the logarithm of the values --> later = log(later, earlier)\n");
+			printf("l: computes the logarithm of the values --> later = "
+				"log(later, earlier)\n");
 			printf("   Note: both values must be positive and error is caught\n");
 			printf("w: swaps earlier and later\n");
 			printf("c: clears the values setting them to 0.\n");
@@ -133,31 +149,35 @@ void calculate(double nums[], char op)
 }
 int main(void)
 {
-	double nums[2] = {0., 0.}; // first element is EARLIER, second is LATER
+	double nums[2] = {0., 0.}; // earlier input, later input
 	char* input = NULL;
-	int max_digits = 3 + DBL_MANT_DIG - DBL_MIN_EXP;
-	input = malloc(max_digits * sizeof(char)); // allocates memory 
-	memset(input, 0, max_digits); // sets all characters to the null char
+	int max_digits = 3 + DBL_MANT_DIG - DBL_MIN_EXP; // max size of double input
+	input = malloc(max_digits * sizeof(char)); // max size of char input
+	memset(input, 0, max_digits); // set string to all null chars
+	// load input into stdin and check for EOF at beginning of file
 	if(fgets(input, max_digits, stdin) != NULL)
 	{
-		while(EOF!=input[0])
+		while(EOF != input[0]) // check next input for EOF
 		{
-			if(input[0] == '\0' || input[0] == '\n')
+			if(input[0] == '\0' || input[0] == '\n') // check empty input
 			{
-				if(fgets(input, max_digits, stdin) == NULL) break;
+				if(fgets(input, max_digits, stdin) == NULL) break; // get next input
 				else continue;
 			}
-			if(!isdigit(input[0]) && input[0] != '-')
+			if(input[0] == 'q') break; // exit if quit is requested
+			if(!isdigit(input[0]) && input[0] != '-') // test non-numerical input
 				calculate(nums, input[0]);
 			else
 			{
+				// update earlier and later store
 				nums[0] = nums[1];
-				nums[1] = atof(input);
+				nums[1] = atof(input); 
 			}
-			memset(input, 0, max_digits);
+			memset(input, 0, max_digits); // clear out input to null chars
 		}
 	}
-	calculate(nums, 'p');
+	calculate(nums, 'p'); // final print before exit
 	free(input);
+
 	return 0;
 }
